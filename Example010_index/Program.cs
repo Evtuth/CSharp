5bc3a8b03ea1f5a678c6468d3289bb7a6f2f5a22
Vasiliy <evtuth@gmail.com>
2022-12-18 18:13:47 +0300

    added Example010 index array`

---
+++ Example010_index/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 121, 232, 233, 434, 2355, 665, 740, 8890 };
+
+int n = array.Length;
+int find = 434;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
